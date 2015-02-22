dc0d045001c1a77f057a705ea95c35aea45431ee
lifebennder <luka.prelicads@gmail.com>
2015-02-22 14:36:07 +0000

    added missing files to git repo

---
+++ static/js/panels.js
@@ -0,0 +1,66 @@
+/**
+ * Created by Lukasz on 21/02/2015.
+ */
+
+var isQuiz = false;
+var correctAnswers = [0, 0, 0, 1, 1, 0, 1, 1, 1];
+window.onload = function (e) {
+
+};
+function hide(id) {
+    d3.select('#' + id).transition().delay(100).duration(1000).style({"opacity": 0, height: '0px'}).remove();
+}
+
+function quiz() {
+    console.log('quiz');
+    //document.getElementsByName('q1').addEventListener("click", validate);
+    // document.getElementById("qqq1").addEventListener("click", validate);
+    d3.selectAll('.radio-inline').on('click', function (d) {
+        validate(this);
+    });
+    //document.getElementById("qqq1")
+    var id = '#quiz';
+    var opacity = 1, width = window.innerWidth * 0.3 + 'px';
+    if (isQuiz) {
+        opacity = 0;
+        width = '0px';
+    }
+    else {
+        d3.select(id).style({"display": 'block'});
+    }
+    // d3.select(id).append(!isQuiz+'  '+width);
+    d3.select(id).transition().duration(1500).style({opacity: opacity, width: width});//.transition().delay(100).duration(1200).style("right",50);
+    if (isQuiz)d3.select(id).transition().delay(1400).style({"display": 'none'});
+    isQuiz = !isQuiz;
+}
+
+function hint(buttonid) {
+    var button = document.getElementById(buttonid);
+    button.click();
+}
+
+function validate(id) {
+    var parent = id.parentNode.id;
+    var radios = -1;
+    if (document.getElementsByName(parent.slice(-2))[0].checked) radios = 0;
+    else if (document.getElementsByName(parent.slice(-2))[1].checked) radios = 1;
+    else return;
+    //console.log(radios);
+    var text = "not set";
+    var isCorrect = radios == correctAnswers[parseInt(parent.slice(-1))];
+    var correct = '✓ Correct';
+    var wrong = '✘ Wrong';
+    //var status = correct;
+    if (isCorrect) {
+        text = correct;
+    }
+    else text = wrong;
+    var statusdiv = d3.select('#' + parent + 'status');
+    if (statusdiv[0][0] != null && statusdiv[0][0] != undefined) {
+        d3.select('#' + parent + 'status').classed({'wrong': !isCorrect, 'correct': isCorrect}).html(text);
+    }
+    else {
+        d3.select('#' + parent).append('div').attr('id', parent + 'status')
+            .classed({'wrong': !isCorrect, 'correct': isCorrect}).html(text);
+    }
+}
